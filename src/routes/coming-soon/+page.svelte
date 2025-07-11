5cc9beec0fb3aef52b14cc322c5617ac45396c67
Guillaume Trüeb <guillaumetrueb@etik.com>
2025-07-11 22:43:59 +0200

    feat: working on the design

---
+++ src/routes/coming-soon/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<div id="hero" class="relative isolate pt-14">
+    <svg class="absolute inset-0 -z-10 size-full mask-[radial-gradient(100%_100%_at_top_right,white,transparent)] stroke-gray-200" aria-hidden="true">
+        <defs>
+            <pattern id="ferum-hero-pattern" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+                <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+        </defs>
+        <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+            <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+        </svg>
+        <rect width="100%" height="100%" stroke-width="0" fill="url(#ferum-hero-pattern)" />
+    </svg>
+    
+    <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+        <div class="mx-auto text-center">
+            <div class="flex-1 flex items-center justify-center mb-8">
+				<a href="/" class="-m-1.5 p-1.5 flex items-center space-x-2">
+					<div class="w-8 h-8 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex items-center justify-center shadow-lg">
+						<span class="text-white font-bold text-sm">F</span>
+					</div>
+					<span class="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">Ferum</span>
+				</a>
+			</div>
+            <div>
+                <h1 class="mt-10 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl">
+                    <span class="bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-transparent">Coming soon (cela arrive bientôt)</span>
+                </h1>
+            </div>
+            <div class="mt-15">
+                <a href="/" class="bg-gradient-to-r from-blue-600 to-purple-600 text-white px-4 py-2 rounded-lg font-semibold text-sm hover:shadow-lg hover:scale-105 transition-all duration-300">
+					Retour à l'accueil
+				</a>
+            </div>
+        </div>
+    </div>
+</div>
